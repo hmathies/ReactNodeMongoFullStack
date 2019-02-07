6d42efd84aa302bbaf46228d19b7f9067a0dd4e6
Heather <hlmathies@gmail.com>
2019-02-06 20:00:11 -0500

    adding comments

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 // prod.js - production keys here
 //these environmental variables are set up on heroku
+// the google credentials are from the google+ API using the OAuth client ID
 
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
