13f9437ca72635a87831099b7cb8a26b1c69c93e
Heather <hlmathies@gmail.com>
2019-02-09 10:02:36 -0500

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -15,12 +15,19 @@ passport.deserializeUser((id, done) => {
   });
 });
 
+//using the relative url means we don't have to worry about the domain that we have to send the user back to
+//it is the google strategy that is deciding which domain to add to the callbackURL whether it be local host or
+//production url so in development it is appending 'localhost:5000' but in heroku it will incorrectly redirect to
+// 'http://nameOfDomain/auth/google/callback' instead of https://nameOfDomain/auth/google/callback due to the heroku proxy
+//to fix it we could add the absolute path using https: || just add `proxy: true` which is telling google to trust the proxy
+// and calculate the callbackURL correctly
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
