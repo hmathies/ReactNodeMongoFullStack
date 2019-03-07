9bf7ce3ea957cc8e9276950b7d687843bdc7040a
Heather <hlmathies@gmail.com>
2019-03-06 19:51:24 -0500

    adding sendgrid click event

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,9 +7,14 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
+  //use 4242 4242 4242 4242 as the card number
   app.get("/api/surveys/thanks", (req, res) => {
     res.send("Thanks for voting!");
   });
+  app.post('/api/surveys/webhooks', (req, res) => {
+    console.log(req.body);
+    res.send({});
+  })
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
