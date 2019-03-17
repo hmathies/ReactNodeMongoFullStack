c8f162f6c728e100cf9098d9ba7e2c9633127f28
Heather <hlmathies@gmail.com>
2019-03-16 22:29:36 -0400

    adding components to display surveys sent

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 // using the reducer function from redux-form. with es6 we can rename using the key work `as`
 import { reducer as reduxForm } from "redux-form";
 import authReducer from "./authReducer";
+import surveysReducer from './surveysReducer';
 
 export default combineReducers({
   auth: authReducer,
-  form: reduxForm
+  form: reduxForm,
+  surveys: surveysReducer
 });
